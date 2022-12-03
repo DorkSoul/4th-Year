acd52bd92f8a9e2bb34d5c51a31435d4d683ec0f
DorkSoul <Luke.hallinan@hotmail.com>
2022-12-03 12:23:56 +0000

    comments and formatting

--- Rich Web/Lab3/Problem-2/javascript/index.js
+++ Rich Web/Lab3/Problem-2/javascript/index.js
@@ -1,64 +1,99 @@
+//set rxjs constants
 const { Observable, fromEvent } = rxjs;
 
+//set constants
 const hoursBox = document.getElementById("hoursBox");
 const minutesBox = document.getElementById("minutesBox");
 const secondsBox = document.getElementById("secondsBox");
 
+//set default state of timer to off
 let state = false;
 
-rxjs.fromEvent(document.getElementById("startButton"), 'click').subscribe(() => stateStartStop());
+//subscribe to the start button to start / /stop the timer
+rxjs.fromEvent(document.getElementById("startButton"), 'click')
+.subscribe(() => stateStartStop());
 
+//function to start stop the timer
 function stateStartStop(){
+    //if timer is not running begin the timer
     if(!state){
 
+        //swap state to on
         state = !state;
 
-        total_time = hoursBox.value * 3600 + minutesBox.value * 60 + parseInt(secondsBox.value);
+        //calculate the total time input by the user in seconds
+        totalTime = parseInt(hoursBox.value) * 3600 + parseInt(minutesBox.value) * 60 + parseInt(secondsBox.value);
         
-        timerObs = rxjs.timer(0, 1000).subscribe(i => TimerDisplay(total_time - i));
+        //subscribe to an rxjs timer to call the timer display and update it every second
+        //total time in seconds is reduced by the amount of times this is run
+        timerObs = rxjs.timer(0, 1000)
+        .subscribe(i => timerDisplay(totalTime - i));
 
+        //change the button to display stop, change its color to red and show the bottom box with the current timer
         document.getElementById("startButton").innerHTML = "Stop";
         document.getElementById("startButton").style.backgroundColor = "red"
         document.getElementById("display").style.visibility = "visible";
 
+        //reset the input fields to 00
         document.getElementById("hoursBox").value = "00";
         document.getElementById("minutesBox").value = "00";
         document.getElementById("secondsBox").value = "00";
         
     }
+    //if timer is already running
     else{
+        //swap state to off
         state = !state;
 
+        //change the button to display start, change its color to green and hide the bottom box with the current timer
         document.getElementById("startButton").innerHTML = "Start";
         document.getElementById("startButton").style.backgroundColor = "lawngreen";
         document.getElementById("display").style.visibility = "hidden";
         
+        //unsubscribe from the rxjs timer as the counddown has been forcefully stopped
         timerObs.unsubscribe();
+        
+        //remove the countdown text and no timer is running
         document.getElementById("timeDisplay").innerHTML = "";
     }
 }
 
-function TimerDisplay(untilEnd){
+//funtion to display the time and check if its finished
+function timerDisplay(totalTime){
 
-    if (untilEnd == 0){
+    //if timer is finished
+    if (totalTime == 0){
+        //unsubscribe from the rxjs timer as the countdown has finished
         timerObs.unsubscribe();
 
+        //change the button to display start, change its color to green and and display the countdown end message
         document.getElementById("startButton").innerHTML = "Start";
         document.getElementById("startButton").style.backgroundColor = "lawngreen";
-        document.getElementById("timeDisplay").innerHTML = "Time's Up!";
+        document.getElementById("timeDisplay").innerHTML = "Countdown has finished";
 
+        //swap state to off
         state = !state;
     }
+    //if timer is running display time until end of countdown
     else{
 
+        //set variables
         let hours, minutes, seconds;
         
-        hours = (untilEnd - untilEnd % 3600) / 3600;
-        untilEnd = untilEnd - hours * 3600;
+        //split the remaining total time into hours, minutes and seconds
+        //calulate hours using modulo remainder to drop minutes / seconds
+        hours = (totalTime - totalTime % 3600) / 3600;
 
-        minutes = (untilEnd - untilEnd % 60) / 60;
-        seconds = untilEnd - minutes * 60;
+        //remove hours from the total time
+        totalTime = totalTime - hours * 3600;
 
+        //calulate minutes using modulo remainder to drop seconds
+        minutes = (totalTime - totalTime % 60) / 60;
+
+        //calculate seconds
+        seconds = totalTime - minutes * 60;
+
+        //display the calulated hours, minutes and seconds
         document.getElementById("timeDisplay").innerHTML = hours + " hours " + minutes + " minutes " + seconds + " seconds";
     }
 }
--- Rich Web/Lab3/Problem-3/javascript/index.js
+++ Rich Web/Lab3/Problem-3/javascript/index.js
@@ -1,12 +1,26 @@
-const { observable, fromEvent, tap } = rxjs;
-  
-function createNote(locationNode) {  
+//set rxjs constants
+const { observable, fromEvent } = rxjs;
+
+//set constants
+const newNote = document.getElementById('newNoteButton')
+
+//subscribe to new note button click to create a new note
+rxjs.fromEvent(newNote, 'click').subscribe(() => {
+    createNote(document.body)
+    })
+
+//fucntion to create new note
+function createNote(createPosition) {
+    //constant to create new div element
     const note = document.createElement('div') 
     
+    //add class note to new div
     note.classList.add('note')  
+
+    //add html to make the note in new div
     note.innerHTML = 
     `
-    <div class="noteGroup">   
+    <div class="noteBackground">   
         <textarea class="noteBody"}></textarea>
         <div class="noteOptions">    
             <button class="delete">delete</button>   
@@ -15,26 +29,20 @@ function createNote(locationNode) {
     </dib>
     `  
 
+    //add delete and create child buttions to new note
     const deleteBtn = note.querySelector('.delete')  
     const createChildBtn = note.querySelector('.createChild')  
 
-    rxjs.fromEvent(deleteBtn, 'click').pipe(rxjs.tap(() => {
+    //subscribe to the new delete button to delete the note
+    rxjs.fromEvent(deleteBtn, 'click').subscribe(() => {
     note.remove() 
-    })).subscribe()
+    })
 
-    rxjs.fromEvent(createChildBtn, 'click').pipe(rxjs.tap(() => {
+    //subscribe to the new create child note button to add a child note
+    rxjs.fromEvent(createChildBtn, 'click').subscribe(() => {
     createNote(note)
-    })).subscribe()
+    })
 
-    locationNode.appendChild(note)  
+    //link the new note to a parent note
+    createPosition.appendChild(note)  
 }  
-    
-    
-function showNotes(){
-    const newNote = document.getElementById('newNoteButton')  
-    rxjs.fromEvent(newNote, 'click').pipe(rxjs.tap(() => {
-    createNote(document.body)
-    })).subscribe()
-}
-
-showNotes()
